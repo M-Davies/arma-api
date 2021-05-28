61f1d97a8e962ec232f8693f4980a99b2a98035a
Morgan Davies <morgandavies2020@gmail.com>
2021-05-28 17:41:26 +0100

    Create initial all classes selection from mongodb

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,28 +1,27 @@
 package com.example.demo;
 
+import java.util.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.DBCursor;
+import org.bson.Document;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-	private static DB database;
-	private DBCollection collection;
+	private static MongoDatabase DATABASE;
 
 
 	public static void main(String[] args) throws UnknownHostException {
-		MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
-		database = mongoClient.getDB("arma-api");
+		MongoClient MONGO_CLIENT = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+		DATABASE = MONGO_CLIENT.getDatabase("arma-api");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
@@ -34,9 +33,20 @@ public class DemoApplication {
 		return String.format("Hello %s!", name);
 	}
 
-	@GetMapping("/ace")
+	@GetMapping("/classes")
+	public String classes() {
+		ArrayList<Document> collectionContents = new ArrayList<Document>();
+		DATABASE.listCollectionNames().forEach(collectionName -> {
+			MongoCollection<Document> currentCollection = DATABASE.getCollection(collectionName);
+			collectionContents.add(currentCollection.find());
+		});
+
+		return String.valueOf(collection.count());
+	}
+
+	@GetMapping("/classes/ace")
 	public String ace() {
-		collection = collection = database.getCollection("data.ace");
+		private DBCollection collection = database.getCollection("data.ace");
 		return String.valueOf(collection.count());
 	}
 
