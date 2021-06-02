5bb724200db42f2b74f00f96802d8cd807cfe2f4
Morgan Davies <morgandavies2020@gmail.com>
2021-06-02 12:54:18 +0100

    Fix and produce working updater version

--- src/main/java/com/api/main/Updater.java
+++ src/main/java/com/api/main/Updater.java
@@ -1,23 +1,24 @@
-package com.example.main;
+package com.api.main;
 
-import java.util.*;
-import java.io.*;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import org.bson.Document;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.UnknownHostException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 public class Updater {
 
-    public static void main(String[] args) throws UnknownHostException, IOException {
+    public static void main(String[] args) throws UnknownHostException, IOException, FileNotFoundException, ParseException {
         // Setup Json parser and Mongodb
         JSONParser PARSER = new JSONParser();
         MongoClient MONGO_CLIENT = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
@@ -25,27 +26,35 @@ public class Updater {
 
         // Iterate over mod Json data files
         Files.list(new File(System.getProperty("user.dir") + "/data").toPath()).forEach(path -> {
+            System.out.println("[INFO] Parsing " + path);
+
             // Set corresponding mongo db collection
             String[] splitPath = path.toString().split("/");
-            String filename = splitPath[splitPath.length - 1];
-            MongoCollection<Document> collection = DATABASE.getCollection("data." + filename);
+            String filename = splitPath[splitPath.length - 1].replace(".json", "");
+            String collectionName = "data." + filename;
+            System.out.println("[INFO] Collection name will be " + collectionName);
+            MongoCollection<Document> collection = DATABASE.getCollection(collectionName);
 
             try {
                 // Read mod JSON file
                 FileReader reader = new FileReader(path.toString());
                 Object obj = PARSER.parse(reader);
-                JSONArray modClassList = (JSONArray) obj;
 
                 // Add to mongo
-                collection.insertOne(Document.parse(modClassList.toString()));
+                collection.insertOne(Document.parse(obj.toString()));
             } catch (FileNotFoundException | ParseException e) {
+                System.out.println("[WARNING] Could not find or read " + path);
                 e.printStackTrace();
             } catch (IOException e) {
+                System.out.println("[WARNING] Could not parse " + path + " to a Object datatype");
                 e.printStackTrace();
             }
+
+            System.out.println("[SUCCESS] " + collectionName + "has been successfully parsed and added to mongo db");
         });
 
         // Close connection
+        System.out.println("[SUCCESS] Parsed and Uploaded all JSON files! Closing connection...");
         MONGO_CLIENT.close();
     }
 
