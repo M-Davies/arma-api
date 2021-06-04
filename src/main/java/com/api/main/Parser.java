2808e43d6f420017d1fd64b0ea4ce2f11754037e
Morgan Davies <morgandavies2020@gmail.com>
2021-06-04 09:13:24 +0100

    Header parsing for biki successfull

--- src/main/java/com/api/main/Parser.java
+++ src/main/java/com/api/main/Parser.java
@@ -1,5 +1,7 @@
 package com.api.main;
 
+import java.util.*;
+import java.util.stream.Collectors;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,26 +10,33 @@ import org.jsoup.select.Elements;
 
 public class Parser {
     public static void main(String[] args) {
-        final String[] pages = {
+        final String[] PAGES = {
             "https://community.bistudio.com/wiki/Arma_3:_CfgWeapons_Weapons",
-            "https://community.bistudio.com/wiki/Arma_3:_CfgMagazines",
             "https://community.bistudio.com/wiki/Arma_3:_CfgWeapons_Items",
-            "https://community.bistudio.com/wiki/Arma_3:_CfgWeapons_Equipment"
+            "https://community.bistudio.com/wiki/Arma_3:_CfgWeapons_Equipment",
+            "https://community.bistudio.com/wiki/Arma_3:_CfgMagazines"
         };
 
-        for (String wikiLink : pages) {
+        final ArrayList<String> IGNORED_HEADERS = new ArrayList<String>(Arrays.asList("Used by", "Objects"));
+
+        for (String wikiLink : PAGES) {
             try {
                 // Grab wiki table from the page
+                System.out.println("[INFO] Downloading " + wikiLink);
                 Document doc = Jsoup.connect(wikiLink).get();
                 Elements tableElements = doc.select("[class*=\"wikitable\"]");
 
-                // Grab header elements
-                Elements tableHeaderEles = tableElements.select("thead tr th");
-                System.out.println("HEADERS FOR" + wikiLink);
-                for (int i = 0; i < tableHeaderEles.size(); i++) {
-                    System.out.println(tableHeaderEles.get(i).text());
-                }
-                System.out.println();
+                // Grab header elements & filter out unneeded ones
+                System.out.println("[INFO] Constructing header schema...");
+                Elements tableHeaderEles = tableElements.select("tbody tr th");
+                ArrayList<Element> tableHeaders = new ArrayList<Element> (
+                    tableHeaderEles
+                    .stream()
+                    .filter(ele -> !IGNORED_HEADERS.contains(ele.text()))
+                    .collect(Collectors.toList())
+                );
+                System.out.println("[SUCCESS] Obtained headers!");
+                tableHeaders.forEach(ele -> System.out.println(ele.text()));
 
                 // Iterate over table rows that are not a header
                 Elements tableRowElements = tableElements.select(":not(thead) tr");
