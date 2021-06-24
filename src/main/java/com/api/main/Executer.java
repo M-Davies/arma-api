fb93c6331361f8b2c0439c27479c83c3a2c0021b
Morgan Davies <morgandavies2020@gmail.com>
2021-06-24 21:52:32 +0100

    Optimisation and Improvement
    * Escape user input of mongo chars
    * Add search route to find a value by a user input search term

--- src/main/java/com/api/main/Executer.java
+++ src/main/java/com/api/main/Executer.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Executer {
     private static MongoDatabase DATABASE;
+    private static ArrayList<Character> SPECIAL_MONGO_CHARS = new ArrayList<Character>(Arrays.asList('\'', '\"', '\\', ';', '{', '}', '$'));
 
     public static void main(String[] args) throws UnknownHostException {
         MongoClient MONGO_CLIENT = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
@@ -39,9 +40,15 @@ public class Executer {
         }
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    private String escapeUserInput(String unfilteredInput) {
+        String filteredInput = "";
+        for (int i = 0; i < unfilteredInput.length(); i++) {
+            char currentCharecter = unfilteredInput.charAt(i);
+            if (!SPECIAL_MONGO_CHARS.contains(currentCharecter)) {
+                filteredInput += currentCharecter;
+            }
+        }
+        return filteredInput;
     }
 
     @GetMapping(value = {"/classes", "/classes/{mod}"})
@@ -49,37 +56,41 @@ public class Executer {
         @PathVariable(required = false, value = "mod") String mod,
         @RequestParam(required = false, value = "type") String type
     ) throws Exception {
+        // Check user input
+        final String filteredMod = mod == "" || mod == null ? "" : escapeUserInput(mod);
+        final String filteredType = type == "" || type == null ? "" : escapeUserInput(type);
+
         // Verify params
-        if (!Config.getMods().contains(mod) && (mod != null && mod != "")) {
+        if (!Config.getMods().contains(filteredMod) && filteredMod != "") {
             throw new Exception("Unidentified mod. Available values are " + Config.getMods().toString());
         }
-        if (!Config.getTypes().contains(type) && (type != null && type != "")) {
+        if (!Config.getTypes().contains(filteredType) && filteredType != "") {
             throw new Exception("Unidentified object type. Available values are " + Config.getTypes().toString());
         }
 
         // Filter db by keywords or return all
         ArrayList<Document> dbContents = new ArrayList<Document>();
         for (String modName : DATABASE.listCollectionNames()) {
-            String parsedModName = "data." + mod;
+            final String parsedModName = "data." + filteredMod;
 
             // Filter by mod
             if (modName.equals(parsedModName)) {
-                MongoCollection<Document> modContents = getCollection(parsedModName);
-                if (type == "" || type == null) {
+                MongoCollection<Document> modContents = getCollection(modName);
+                if (filteredType.isEmpty()) {
                     modContents.find().into(dbContents);
                 } else {
-                    modContents.find(Filters.eq("type", type)).into(dbContents);
+                    modContents.find(Filters.eq("type", filteredType)).into(dbContents);
                 }
 
                 // Break so we only return a singular mod spec
                 break;
-            } else if (mod == "" || mod == null) {
+            } else if (filteredMod.isEmpty()) {
                 // Get all mods
                 MongoCollection<Document> modContents = getCollection(modName);
-                if (type == "" || type == null) {
+                if (filteredType.isEmpty()) {
                     modContents.find().into(dbContents);
                 } else {
-                    modContents.find(Filters.eq("type", type)).into(dbContents);
+                    modContents.find(Filters.eq("type", filteredType)).into(dbContents);
                 }
             }
         }
@@ -92,6 +103,41 @@ public class Executer {
             .sorted()
             .collect(Collectors.toList())
         );
+    }
+
+    @GetMapping(value = {"/classes/search/{term}"})
+    public String className (
+        @PathVariable(required = true, value = "term") String term
+    ) throws Exception {
+        // Escape user input
+        final String filteredTerm = escapeUserInput(term);
+
+        // Match using Bson filter
+        ArrayList<Document> matchedClasses = new ArrayList<Document>();
+        for (String modName : DATABASE.listCollectionNames()) {
+            ArrayList<Document> filteredContents = new ArrayList<Document>();
+            try {
+                getCollection(modName).find(
+                    Filters.eq("count", Long.parseLong(filteredTerm))
+                ).into(filteredContents);
+            } catch (NumberFormatException e) {
+                getCollection(modName).find(Filters.text(filteredTerm)).into(filteredContents);
+            }
+
+            // Add to final doc if match is found
+            if (filteredContents != null) {
+                matchedClasses.addAll(filteredContents);
+            }
+        }
+
+        return String.valueOf(matchedClasses
+            .stream()
+            .distinct()
+            .map(typeDoc -> typeDoc.toJson())
+            .sorted()
+            .collect(Collectors.toList())
+        );
+
     }
 
 }
