c8b7fdacbeee248af511adbcd9791888d83ede63
Morgan Davies <morgandavies2020@gmail.com>
2021-09-06 23:15:59 +0100

    Fix updater backup logic and connection closing

--- src/main/java/com/api/main/Updater.java
+++ src/main/java/com/api/main/Updater.java
@@ -10,7 +10,6 @@ import java.util.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.*;
@@ -28,73 +27,84 @@ public class Updater {
         final JSONParser PARSER = new JSONParser();
         final MongoClient MONGO_CLIENT = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
         final MongoDatabase DATABASE = MONGO_CLIENT.getDatabase("arma-api");
+        final MongoDatabase BACKUP_DATABASE = MONGO_CLIENT.getDatabase("arma-api-backup");
 
-        // Backup and reset db
-        MongoIterable<String> collections = DATABASE.listCollectionNames();
-        for (String collectionName : collections) {
-            final String backupCollectionName = collectionName + ".BACKUP";
-            System.out.println("[INFO] Backing up and resetting " + collectionName + " to " + backupCollectionName);
+        try {
+            // Backup and reset db
+            MongoIterable<String> collections = DATABASE.listCollectionNames();
+            for (String collectionName : collections) {
+                System.out.println("[INFO] Backing up and resetting " + collectionName);
 
-            // Delete existing backup if it exists
-            DATABASE.getCollection(backupCollectionName).drop();
+                // Delete existing valid backup if it exists
+                MongoCollection<Document> backupCollection = BACKUP_DATABASE.getCollection(collectionName);
+                if (backupCollection.estimatedDocumentCount() > 0) {
+                    backupCollection.deleteMany(new Document());
+                }
 
-            // Pipeline
-            ArrayList<BasicDBObject> pipelineOps = new ArrayList<BasicDBObject>(
-                Arrays.asList(new BasicDBObject("$out", backupCollectionName))
-            );
+                // Copy contents of collection to backup
+                MongoCollection<Document> prodCollection = DATABASE.getCollection(collectionName);
+                ArrayList<Document> prodContents = new ArrayList<Document>();
+                prodCollection.find().into(prodContents);
+                if (prodContents.size() > 0) {
+                    backupCollection.insertMany(prodContents);
+                }
 
-            // Copy contents of collection to backup
-            MongoCollection<Document> collection = DATABASE.getCollection(collectionName);
-            collection.aggregate(pipelineOps);
-
-            // Delete contents of prod collection
-            collection.deleteMany(new Document());
-            collection.dropIndexes();
-        }
-
-        Files.list(new File(System.getProperty("user.dir") + "/data").toPath()).forEach(path -> {
-            System.out.println("[INFO] Parsing " + path);
-            try {
-                // Read JSON file
-                final JSONArray jsonData = (JSONArray) PARSER.parse(new FileReader(path.toString()));
-                jsonData.forEach(configStr -> {
-                    try {
-                        // Parse json string
-                        final JSONObject configObj = (JSONObject) configStr;
-
-                        // Create mongo document
-                        final Document configDoc = new Document(
-                            new ObjectMapper().readValue(configObj.toJSONString(), HashMap.class)
-                        );
-
-                        // Calculate target collection based off mod and append
-                        DATABASE.getCollection("data." + configObj.get("mod").toString())
-                            .insertOne(configDoc);
-                    } catch (JsonProcessingException e) {
-                        System.out.println("[WARNING] Could not create mongo document from config object:\n" + configStr);
-                        e.printStackTrace();
-                    }
-                });
-            } catch (FileNotFoundException | ParseException e) {
-                System.out.println("[WARNING] Could not find or read " + path);
-                e.printStackTrace();
-            } catch (IOException e) {
-                System.out.println("[WARNING] Could not parse " + path + " to a JSON Array");
-                e.printStackTrace();
+                // Delete contents of prod collection
+                prodCollection.deleteMany(new Document());
+                prodCollection.dropIndexes();
             }
-            System.out.println("[SUCCESS] " + path + " successfully parsed and added to mongo db!");
-        });
 
-        // Update indices
-        System.out.println("[INFO] Updating indices for all collections...");
-        collections = DATABASE.listCollectionNames();
-        for (String collectionName : collections) {
-            DATABASE.getCollection(collectionName).createIndex(Indexes.text());
+            // In final backup stage, create the indexes
+            for (String backupCollectionName : BACKUP_DATABASE.listCollectionNames()) {
+                MongoCollection<Document> backupCollection = BACKUP_DATABASE.getCollection(backupCollectionName);
+                backupCollection.dropIndexes();
+                backupCollection.createIndex(Indexes.text());
+            }
+
+            Files.list(new File(System.getProperty("user.dir") + "/data").toPath()).forEach(path -> {
+                System.out.println("[INFO] Parsing " + path);
+                try {
+                    // Read JSON file
+                    final JSONArray jsonData = (JSONArray) PARSER.parse(new FileReader(path.toString()));
+                    jsonData.forEach(configStr -> {
+                        try {
+                            // Parse json string
+                            final JSONObject configObj = (JSONObject) configStr;
+
+                            // Create mongo document
+                            final Document configDoc = new Document(
+                                new ObjectMapper().readValue(configObj.toJSONString(), HashMap.class)
+                            );
+
+                            // Calculate target collection based off mod and append
+                            DATABASE.getCollection("data." + configObj.get("mod").toString())
+                                .insertOne(configDoc);
+                        } catch (JsonProcessingException e) {
+                            System.out.println("[WARNING] Could not create mongo document from config object:\n" + configStr);
+                            e.printStackTrace();
+                        }
+                    });
+                } catch (FileNotFoundException | ParseException e) {
+                    System.out.println("[WARNING] Could not find or read " + path);
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    System.out.println("[WARNING] Could not parse " + path + " to a JSON Array");
+                    e.printStackTrace();
+                }
+                System.out.println("[SUCCESS] " + path + " successfully parsed and added to mongo db!");
+            });
+
+            // Update indices
+            System.out.println("[INFO] Updating indices for all collections...");
+            collections = DATABASE.listCollectionNames();
+            for (String collectionName : collections) {
+                DATABASE.getCollection(collectionName).createIndex(Indexes.text());
+            }
+        } finally {
+            // Close connection
+            System.out.println("[INFO] Closing connection...");
+            MONGO_CLIENT.close();
         }
-
-        // Close connection
-        System.out.println("[SUCCESS] Parsed and Uploaded all JSON files! Closing connection...");
-        MONGO_CLIENT.close();
     }
 
 }
